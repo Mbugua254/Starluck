5c1e2d6a60e9aa1c118a308d0c839a466fca93a0
Michael Mbugua <mikembugua254@gmail.com>
2025-05-04 16:24:33 +0300

    frontend corrections

--- starluck-frontend/src/components/Register.js
+++ starluck-frontend/src/components/Register.js
@@ -18,7 +18,7 @@ function Register() {
   function handleSubmit(e) {
     e.preventDefault();
 
-    fetch('http://localhost:5000/users', {
+    fetch('https://starlucktours.onrender.com/users', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(formData)
